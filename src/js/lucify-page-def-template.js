d17eebb75bcc336ddd058e8bf706d3d6994815a2
Juho Ojala <juho@lucify.com>
2015-10-24 11:10:02 +0300

    Add page metadata template

---
+++ src/js/lucify-page-def-template.js
@@ -0,0 +1,18 @@
+
+var extend = require('object-extend');
+
+var base = {
+	twitterSite: "@lucify_",
+	twitterCreator: "@lucify",
+	ogType: 'w',
+	ogSiteName: 'Lucify',
+	fbAppId: 1383541931953557,
+}
+
+
+var apply = function(def) {
+	return extend(base, def);
+}
+
+module.exports.base = base;
+module.exports.apply = apply;

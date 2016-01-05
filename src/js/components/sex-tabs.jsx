9f26548ddd08db3f5e896bbafbfdba42fc3e1581
Juho Ojala <juho@lucify.com>
2016-01-05 13:22:35 +0200

    Add sex-tabs component

---
+++ src/js/components/sex-tabs.jsx
@@ -0,0 +1,65 @@
+
+var React = require('react');
+var Tabs = require('react-simpletabs');
+
+
+module.exports = React.createClass({
+
+
+  getInitialState: function() {
+    return {
+      index: 1
+    }
+  },
+
+  onChange: function(index) {
+    this.setState({index: index});
+    if (this.props.onChange) {
+      //console.log(this.getPos(index));
+      this.props.onChange(this.getPos(index));
+    }
+  },
+
+
+  getPos: function(index) {
+    switch (index) {
+      case 1: return 'a';
+      case 2: return 'f';
+      case 3: return 'm';
+      case 4: return 'l';
+    }
+    return 'a';
+  },
+
+
+  render: function() {
+    
+    var style = {
+        backgroundColor: "transparent",
+        borderBottom: "1px solid transparent",
+        maxHeight: 12
+    }
+
+    var tabStyle = {
+      color: "black",
+      textAlign: "left",
+      height: 24
+    }
+
+    return (
+        <div className="sex-tabs">
+          <div>
+              <Tabs onAfterChange={this.onChange} tabActive={this.state.index}> 
+                <Tabs.Panel title="Kaikki"><div /></Tabs.Panel>
+                <Tabs.Panel title="Naiset"><div /></Tabs.Panel>
+                <Tabs.Panel title="Miehet"><div /></Tabs.Panel>
+                <Tabs.Panel title="Looppi"><div /></Tabs.Panel>
+              </Tabs> 
+          </div>
+        </div>
+    )
+  }
+
+
+});
+
---
+++ src/js/components/with-sex-tabs.jsx
@@ -0,0 +1,36 @@
+
+
+var React = require('react');
+var SexTabs = require('./sex-tabs.jsx');
+var bindToSexLoopContext = require('./bind-to-sex-loop-context.jsx');
+var Hider = require('./element-query-hider.jsx');
+
+
+module.exports = function(Component, minWidth) {
+
+  return bindToSexLoopContext(React.createClass({
+
+    render: function() {
+
+        if (!minWidth) {
+          return (
+             <div>
+               <SexTabs onChange={this.props.onSexSelectionChange} />
+               <Component {...this.props} sex={this.props.sex} />
+             </div>
+          )
+        };
+
+        return (
+          <div>
+            <Hider minWidth={minWidth}>
+              <SexTabs onChange={this.props.onSexSelectionChange} />
+            </Hider>
+            <Component {...this.props} sex={this.props.sex} />
+          </div>
+        );
+    }
+
+  }));
+
+};

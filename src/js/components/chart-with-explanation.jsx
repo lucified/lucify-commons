fe9e6a9c38ff1827bb268a466da87c5feaf6cabb
Juho Ojala <juho@lucify.com>
2016-01-05 18:53:11 +0200

    Add some components from lucify-website

---
+++ src/js/components/chart-with-explanation.jsx
@@ -0,0 +1,53 @@
+
+var React = require('react');
+var HideableContainer = require('lucify-commons/src/js/components/hideable-container.jsx');
+
+var OnResize = require("react-window-mixins").OnResize;
+var DividedCols = require('lucify-commons/src/js/components/divided-cols.jsx');
+
+var ComponentWidthMixin = require('lucify-commons/src/js/components/container-width-mixin.js');
+
+
+var ChartWithExplanation = React.createClass({
+
+  mixins: [ ComponentWidthMixin ],
+
+
+  getContents: function() {
+    return <DividedCols
+      first={this.props.children[0]}
+      second={this.props.children[1]}
+      inverse={true} />
+  },
+
+
+  getClassName: function() {
+    if (this.componentWidth < 700) {
+      return "chart-with-explanation--stacked";
+    }
+
+    if (this.componentWidth < 800) {
+      return "chart-with-explanation--narrow";
+    }
+
+    return "chart-with-explanation";
+  },
+
+  render: function() {
+
+   
+      return (
+          <HideableContainer 
+          visible={this.props.visible} 
+          delay={this.props.delay}
+          removeFromDOM={true}>
+          <div className={this.getClassName()}>
+            {this.getContents()}
+          </div>
+        </HideableContainer>
+      );
+  }
+
+});
+
+module.exports = ChartWithExplanation;
---
+++ src/js/components/more-info.jsx
@@ -0,0 +1,33 @@
+
+var React = require('react');
+
+
+module.exports = React.createClass({
+
+
+  getInitialState: function() {
+    return {
+      opened: false
+    }
+  },
+
+
+  onClick: function() {
+    console.log("clicked");
+    this.setState({opened: true});
+  },
+
+
+  render: function() {
+    if (!this.state.opened) {
+      return (
+        <a onClick={this.onClick}>{this.props.initial}</a>
+      );
+    
+    } else {
+      return <span>{this.props.children}</span>;
+    }
+  }
+
+
+});

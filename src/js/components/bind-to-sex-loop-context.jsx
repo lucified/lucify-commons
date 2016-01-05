c82f79e27e28b7535db93b9e9f4c7ac082ef9c93
Juho Ojala <juho@lucify.com>
2016-01-05 14:47:07 +0200

    Add missing file

---
+++ src/js/components/bind-to-sex-loop-context.jsx
@@ -0,0 +1,62 @@
+
+
+var React = require('react');
+var d3 = require('d3');
+
+
+module.exports = function(Component) {
+
+  return React.createClass({
+
+
+    getInitialState: function() {
+      return {
+        sex: 'a',
+        sexSelection: 'a'};
+    },
+
+
+    onSexSelectionChange: function(sel) {
+      if (sel != 'l') {
+        clearInterval(this.interval);
+      }
+      
+      if (sel == 'l') {
+        this.setState({sex: 'f'});
+        this.startLoop();
+        this.setState({sexSelection: sel});
+
+      } else {
+        this.setState({sexSelection: sel, sex: sel});
+
+      }
+    },
+
+
+    startLoop: function() {
+      this.toggle();
+      this.interval = setInterval(this.toggle, 1100);
+    },
+
+
+    toggle: function() {
+      if (this.state.sex == 'f') {
+        this.setState({sex: 'm'});
+        return;
+      }
+      if (this.state.sex == 'm') {
+        this.setState({sex: 'f'});
+        return;
+      }
+    },
+
+
+    render: function() {
+      return <Component {...this.props} sex={this.state.sex} 
+        onSexSelectionChange={this.onSexSelectionChange} />
+    }
+
+  });
+
+}
+

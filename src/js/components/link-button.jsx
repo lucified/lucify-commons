40e5a8eef4b3cae0691218b9570ab82c4da9d959
Juho Ojala <juho@lucify.com>
2016-01-06 19:31:55 +0200

    Add link-button from lucify-website

---
+++ src/js/components/link-button.jsx
@@ -0,0 +1,18 @@
+
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
+
+module.exports = React.createClass({
+
+  render: function() {
+
+    return <Link to={this.props.to}>
+        <span className="nice-button">{this.props.children}</span>
+      </Link>
+
+  }
+
+});
+
